451834a61262029b8b23e74c76cdf8e7b06a4449
David Silva <david_ripeace@hotmail.com>
2025-10-04 22:08:42 +0200

    improve skill slot code

--- the-collector/skill_slot.gd
+++ the-collector/skill_slot.gd
@@ -1,7 +1,6 @@
 extends TextureRect
 
 @export var skill: GlobalSignals.SkillType
-
 var skill_icon: Texture2D = null
 
 func _ready():
@@ -11,19 +10,19 @@ func _ready():
 func _refresh_visual():
 	if skill_icon:
 		texture = skill_icon
+		
+func apply_card(data: Dictionary) -> void:
+	# Same logic you do in _drop_data, but callable from code
+	GlobalSignals.emit_signal("unequip_skill", skill)
+	skill = data.get("skill")
+	skill_icon = data.get("skill_icon")
+	$Name.text = data.get("name")
+	GlobalSignals.emit_signal("equip_skill", skill)
+	_refresh_visual()
 
 func _drop_data(at_position, data):
 	if data.get("source_type","") == "card":
-		GlobalSignals.emit_signal("unequip_skill", skill)
-		skill = data.get("skill")
-		skill_icon = data.get("skill_icon")
-		$Name.text = data.get("name")
-		GlobalSignals.emit_signal("equip_skill", skill)
-		_refresh_visual()
+		apply_card(data)
 
 func _can_drop_data(at_position, data):
-	if typeof(data) != TYPE_DICTIONARY:
-		return false
-	if data.get("source_type","") == "card":
-		return true
-	return false
+	return typeof(data) == TYPE_DICTIONARY and data.get("source_type","") == "card"
