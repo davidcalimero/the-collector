e9628b7f56740d7fa519ea96c93c0383ffaba85d
David Silva <david_ripeace@hotmail.com>
2025-10-04 19:57:48 +0200

    start screen game pad support

--- the-collector/start_game_button.gd
+++ the-collector/start_game_button.gd
@@ -4,6 +4,11 @@ extends Button
 
 func _ready():
 	Music.play_music(load("res://assets/audio/music.mp3"))
+	grab_focus()
 
 func _on_button_pressed():
 	get_tree().change_scene_to_file(target_scene_path)
+	
+func _unhandled_input(event):
+	if event.is_action_pressed("ui_accept") and has_focus():
+		_on_button_pressed()
