c35b9d3a56ae0f95a9fb743a3fc400d98afe8e4a
David Silva <david_ripeace@hotmail.com>
2025-10-04 18:13:56 +0200

    new card ui

--- the-collector/skill_slot.gd
+++ the-collector/skill_slot.gd
@@ -1,23 +1,22 @@
 extends TextureRect
 
-@export var empty_icon: Texture2D
-@export var skill_icon: Texture2D
 @export var skill: GlobalSignals.SkillType
-@export var has_skill = false
+
+var skill_icon: Texture2D = null
 
 func _ready():
 	stretch_mode = TextureRect.STRETCH_KEEP_ASPECT_CENTERED
 	_refresh_visual()
 
 func _refresh_visual():
-	texture = skill_icon if has_skill else empty_icon
+	if skill_icon:
+		texture = skill_icon
 
 func _drop_data(at_position, data):
 	if data.get("source_type","") == "card":
 		GlobalSignals.emit_signal("unequip_skill", skill)
 		skill = data.get("skill")
 		skill_icon = data.get("skill_icon")
-		has_skill = true
 		GlobalSignals.emit_signal("equip_skill", skill)
 		_refresh_visual()
 
