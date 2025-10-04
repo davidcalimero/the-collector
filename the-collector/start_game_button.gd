97fcf586f3c32c5ad46c8a20ed64dc75f5650605
David Silva <david_ripeace@hotmail.com>
2025-10-04 13:02:08 +0200

    use connection signals on start menu

--- the-collector/start_game_button.gd
+++ the-collector/start_game_button.gd
@@ -2,8 +2,5 @@ extends Button
 
 @export var target_scene_path: String = "res://main_scene.tscn"
 
-func _ready():
-	self.pressed.connect(_on_button_pressed)
-
 func _on_button_pressed():
 	get_tree().change_scene_to_file(target_scene_path)
